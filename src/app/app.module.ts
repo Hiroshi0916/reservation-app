5de72578340e26e10130b1a1d04e4bc3a1a37d40
Hiroshi <hiroshi.obata@gmail.com>
2022-12-31 20:52:11 -0800

    Angular frontend create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 
+
+import { ProductComponent } from './product/product.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product/product-listings/product-listings.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {products} from '../../products';
+
+@Component({
+  selector: 'app-product-listings',
+  templateUrl: './product-listings.component.html',
+  styleUrls: ['./product-listings.component.scss']
+})
+export class ProductListComponent{
+   
+  products:any=[1,2,3,4]
+
+  constructor(){}
+  ngOnInit(){
+    this.products=products
+  }
+
+}
